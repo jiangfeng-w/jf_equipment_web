7cd944bccfca9b83666992b7e108c687aeb746bd
ankh <1832697406@qq.com>
2023-04-25 19:37:03 +0800

    下载了所需要的包，包括axios，dayjs，element-plus； 开发环境下的unplugin-vue-components unplugin-auto-import，用于自动按需导入； 删除了多余的自带文件，配置了main.js和vue.config.js，添加了element-plus自动导入，反向代理实现跨域

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// element-plus中文配置
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(ElementPlus, {
+        locale: zhCn,
+    })
+    .mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
+// element-plus自动引入
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    lintOnSave: false, // 关闭语法检查
+    // element自动引入
+    configureWebpack: {
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
+    },
+    // 跨域
+    devServer: {
+        proxy: {
+            '/web': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+        },
+    },
 })
