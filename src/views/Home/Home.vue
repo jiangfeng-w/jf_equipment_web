d5d1bbccd994d77c5dd2394e53e5da07c0b083fb
ankh <1832697406@qq.com>
2023-04-30 22:16:08 +0800

    从数据库获取预约列表，替换掉最开始的预约列表假数据

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -17,7 +17,7 @@
                         table-layout="fixed"
                     >
                         <el-table-column
-                            prop="book_name"
+                            prop="apply_name"
                             label="预约人"
                             width="100"
                         ></el-table-column>
@@ -26,12 +26,17 @@
                             width="165"
                         >
                             <template #default="scope">
-                                {{ formatTime(scope.row.book_time) }}
+                                {{ formatTime(scope.row.apply_time) }}
                             </template>
                         </el-table-column>
                         <el-table-column
-                            prop="device_name"
+                            prop="name"
                             label="设备名称"
+                        ></el-table-column>
+                        <el-table-column
+                            prop="manager_name"
+                            label="设备负责人"
+                            width="100"
                         ></el-table-column>
                         <el-table-column
                             label="当前状态"
@@ -80,69 +85,18 @@
     import axios from 'axios'
     import dayjs from 'dayjs'
 
+    // 获取预约列表
+    const getBookList = async () => {
+        const res = await axios.get('/web/equipment/getBookList')
+        // console.log(res.data.data)
+        tableList.splice(0, tableList.length, ...res.data.data)
+    }
+    onMounted(() => {
+        getBookList()
+    })
+
     // 预约表格
-    const tableList = reactive([
-        {
-            book_name: '张三',
-            book_time: 1651234567890,
-            device_name: '高压气相色谱仪',
-            state: 1,
-        },
-        {
-            book_name: '李四',
-            book_time: 1652345678901,
-            device_name: '原位变温高压激光共聚焦显微拉曼光谱仪',
-            state: 2,
-        },
-        {
-            book_name: '王五',
-            book_time: 1653456789012,
-            device_name: '电子万能试验机',
-            state: 0,
-        },
-        {
-            book_name: '赵六',
-            book_time: 1654567890123,
-            device_name: '高速离心机',
-            state: 1,
-        },
-        {
-            book_name: '钱七',
-            book_time: 1655678901234,
-            device_name: '气相色谱-质谱联用仪',
-            state: 2,
-        },
-        {
-            book_name: '孙八',
-            book_time: 1656789012345,
-            device_name: '电子显微镜',
-            state: 0,
-        },
-        {
-            book_name: '周九',
-            book_time: 1657890123456,
-            device_name: '原子力显微镜',
-            state: 1,
-        },
-        {
-            book_name: '吴十',
-            book_time: 1658901234567,
-            device_name: '高压液相色谱仪',
-            state: 2,
-        },
-        {
-            book_name: '郑十一',
-            book_time: 1660012345678,
-            device_name: '红外光谱仪',
-            state: 0,
-        },
-        {
-            book_name: '王十二',
-            book_time: 1661123456789,
-            device_name: '电化学工作站',
-            state: 1,
-        },
-    ])
+    const tableList = reactive([])
     // 排行榜
     const rangList = reactive([
         {
@@ -216,6 +170,7 @@
         // height: 100%;
         height: 80vh;
         padding: 0;
+        overflow: auto;
     }
     // #region 排行榜的样式
     .rank-list {
